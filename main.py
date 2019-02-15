0137bee534912cd41216876219ad7830f7ea0dff
Péridot Shiyun <33402900+Fukiyel@users.noreply.github.com>
2019-02-15 17:39:55 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from tree import *
+
+r0 = Root()
+n0, n1, n2, n3 = Node(r0),  Node(r0), Node(r0), Node(r0)
+n1_0 = Node(n1)
+n1_0_0, n1_0_1, n1_0_2 = Node(n1_0),  Node(n1_0),  Node(n1_0)
+
+r0.displayTree()
